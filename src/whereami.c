472e0f6fa95daebc421a0e781c3da2cd97e36c30
yangfl <yangfl@users.noreply.github.com>
2020-04-10 00:08:40 +0800

    added support for kFreeBSD and GNU Hurd
    Ref: https://github.com/marcIhm/yabasic/pull/29

--- src/whereami.c
+++ src/whereami.c
@@ -158,7 +158,9 @@ int WAI_PREFIX(getModulePath)(char* out, int capacity, int* dirname_length)
   return length;
 }
 
-#elif defined(__linux__) || defined(__CYGWIN__) || defined(__sun) || defined(WAI_USE_PROC_SELF_EXE)
+#elif defined(__linux__) || defined(__CYGWIN__) || defined(__sun) || defined(__GNU__) || defined(WAI_USE_PROC_SELF_EXE)
+
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -179,6 +181,10 @@ int WAI_PREFIX(getModulePath)(char* out, int capacity, int* dirname_length)
 #else
 #define WAI_PROC_SELF_EXE "/proc/self/exe"
 #endif
+#endif
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
 #endif
 
 WAI_FUNCSPEC
